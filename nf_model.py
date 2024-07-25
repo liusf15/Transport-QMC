e780bbad7a3db02704bd8c35f1d002a95c3cfbcc
Sifan <sfliu@stanford.edu>
2024-07-25 09:20:36 -0400

    init

---
+++ nf_model.py
@@ -0,0 +1,220 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import qmc
+from tqdm import trange
+
+import jax.numpy as jnp
+from jax.scipy.special import betainc, ndtri, ndtr
+from jax.scipy.stats.beta import pdf as beta_pdf
+from jax.nn import softmax
+from jax.scipy.linalg import det
+import jax
+import optax
+
+from experiment.polynomial.targets import Gaussian, LogNormal
+MACHINE_EPSILON = np.finfo(np.float64).eps
+
+def bernstein_poly(x, deg, weights):
+    return jnp.dot(betainc(jnp.arange(1, deg + 1), jnp.arange(deg, 0, -1), x), weights)
+
+def bernstein_poly_grad(x, deg, weights):
+    return jnp.dot(beta_pdf(x, jnp.arange(1, deg + 1), jnp.arange(deg, 0, -1)), weights)
+
+class Copula_NF():
+    def __init__(self, d, target, max_deg) -> None:
+        self.d = d
+        self.target = target
+        self.max_deg = max_deg
+    
+    def init_params(self):
+        self.mu = jnp.zeros(self.d)
+        self.L = jnp.eye(self.d)
+        self.weights_ = jnp.zeros((self.d, self.max_deg))
+        self.params = self.pack_params()
+    
+    def pack_params(self):
+        return jnp.concatenate([self.mu, self.L.flatten(), self.weights_.flatten()])
+    
+    def set_params(self, params):
+        self.mu = params[:self.d]
+        self.L = params[self.d:self.d+self.d**2].reshape(self.d, self.d)
+        self.weights_ = params[self.d+self.d**2:].reshape(self.d, self.max_deg)
+        self.params = params
+
+    def forward_and_logdet(self, x, link_func=['sigmoid', 'logit']):
+        """
+        x: (n, d)
+        link_func: function that maps R to [0, 1] and [0, 1] to the desired range
+        """
+        t_x = self.mu + x @ self.L.T
+        log_det = jnp.log(det(self.L @ self.L.T)) / 2
+        
+        if link_func[0] == 'ndtr':
+            # use \Phi to map R to [0, 1]
+            log_det += jnp.sum(-.5 * t_x**2 - .5 * jnp.log(2 * jnp.pi), axis=1)
+            t_x = ndtr(t_x)
+            t_x = jnp.clip(t_x, MACHINE_EPSILON, 1 - MACHINE_EPSILON) # avoid numerical instability
+
+        elif link_func[0] == 'sigmoid':
+            # use sigmoid to map R to [0, 1]
+            log_det += jnp.sum(-t_x - jnp.log(1 + jnp.exp(-t_x)) * 2., axis=1)
+            t_x = 1 / (1 + jnp.exp(-t_x))
+
+        else:
+            raise ValueError(f"Unknown link function: {link_func[0]}")
+
+        output = []
+        for j in range(self.d):
+            weight_j = softmax(self.weights_[j])
+            output.append(bernstein_poly(t_x[:, j:j+1], deg=self.max_deg, weights=weight_j))
+            log_det += jnp.log(bernstein_poly_grad(t_x[:, j:j+1], deg=self.max_deg, weights=weight_j))
+        output = jnp.stack(output, axis=1)
+
+        if link_func[1] == 'ndtri':
+            # use \Phi^{-1} to map [0, 1] to R
+            output = ndtri(output)
+            log_det += jnp.sum(+.5 * output**2 + .5 * jnp.log(2 * jnp.pi), axis=1)
+
+        elif link_func[1] == 'logit':
+            # use logit to map [0, 1] to R
+            output = jnp.clip(output, MACHINE_EPSILON * .5, 1 - MACHINE_EPSILON * .5)
+            log_det += jnp.sum(-jnp.log(output) - jnp.log(1 - output), axis=1)
+            output = jnp.log(output / (1 - output))
+
+        # use log (1/(1-x)) to map [0,1] to R_+
+        elif link_func[1] == 'positive_range':
+            log_det += jnp.sum(-jnp.log(1 - output), axis=1)
+            output = -jnp.log(1 - output)
+        
+        else:
+            raise ValueError(f"Unknown link function: {link_func[1]}")
+
+        return output, log_det
+    
+    def reverse_kld(self, params, x):
+        """
+        x: (num_samples, d)
+        """
+        self.set_params(params)
+        log_q = -0.5 * jnp.sum(x**2, axis=-1) - 0.5 * self.d * jnp.log(2 * jnp.pi)
+        z, log_det = self.forward_and_logdet(x)
+        log_p = self.target.log_prob(z)
+        return jnp.mean(log_q - log_det - log_p)
+    
+    def optimize(self, max_iter=50, lr=None, nsample=2**10, seed=0):
+        optimizer = optax.lbfgs(learning_rate=lr)
+        params = self.pack_params()
+        opt_state = optimizer.init(params)
+
+        @jax.jit
+        def step(params, opt_state, x):
+            loss, grad = jax.value_and_grad(self.reverse_kld)(params, x)
+            updates, opt_state = optimizer.update(grad, opt_state, params, value=loss, grad=grad, value_fn=lambda params: self.reverse_kld(params, x))
+            params = optax.apply_updates(params, updates)
+            return params, opt_state, loss
+
+        soboleng = qmc.Sobol(self.d, scramble=True, seed=seed)
+        X = ndtri(soboleng.random(nsample) * (1 - MACHINE_EPSILON) + .5 * MACHINE_EPSILON)
+        losses = []
+        pbar = trange(max_iter)
+        for t in pbar:
+            params, opt_state, loss = step(params, opt_state, X)
+            losses.append(loss.item())
+            pbar.set_description(f"Loss: {loss.item()}")
+        self.set_params(params)
+        log_q = -0.5 * jnp.sum(X**2, axis=-1) - 0.5 * self.d * jnp.log(2 * jnp.pi)
+        Z_nf, log_det = self.forward_and_logdet(X)
+        log_p = self.target.log_prob(Z_nf)
+        weights = np.exp(log_p - log_q + log_det)
+        return Z_nf, weights, losses
+    
+    def _optimize_eval(self, max_iter=50, lr=None, rqmc=True, nsample=2**10, seed=0):
+        optimizer = optax.lbfgs(learning_rate=lr)
+        params = self.pack_params()
+        opt_state = optimizer.init(params)
+
+        @jax.jit
+        def step(params, opt_state, x):
+            loss, grad = jax.value_and_grad(self.reverse_kld)(params, x)
+            updates, opt_state = optimizer.update(grad, opt_state, params, value=loss, grad=grad, value_fn=lambda params: self.reverse_kld(params, x))
+            params = optax.apply_updates(params, updates)
+            return params, opt_state, loss
+
+        if rqmc:
+            soboleng = qmc.Sobol(self.d, scramble=True, seed=seed)
+            X = ndtri(soboleng.random(nsample) * (1 - MACHINE_EPSILON) + .5 * MACHINE_EPSILON)
+        else:
+            rng = np.random.default_rng(seed)
+            X = rng.standard_normal((nsample, self.d))
+        losses = []
+        evals = []
+        pbar = trange(max_iter)
+        for t in pbar:
+            params, opt_state, loss = step(params, opt_state, X)
+            losses.append(loss.item())
+            pbar.set_description(f"Loss: {loss.item()}")
+
+            self.set_params(params)
+            log_q = -0.5 * jnp.sum(X**2, axis=-1) - 0.5 * self.d * jnp.log(2 * jnp.pi)
+            Z_nf, log_det = self.forward_and_logdet(X)
+            log_p = self.target.log_prob(Z_nf)
+            weights = np.exp(log_p - log_q + log_det)
+            mean_est = np.mean(Z_nf * weights[:, None], 0) / np.mean(weights)
+            var_est = np.mean(Z_nf**2 * weights[:, None], 0) / np.mean(weights)
+            err_1 = np.mean((mean_est - self.true_mean)**2).item()
+            err_2 = np.mean((var_est - self.true_var)**2).item()
+            evals.append((err_1, err_2))
+        return Z_nf, weights, losses, evals
+    
+
+    def sample(self, n, seed=0, rqmc=True, return_weights=False):
+        """
+        seed: either integer seed or a numpy random generator
+        """
+        if not rqmc:
+            rng = np.random.default_rng(seed)
+            X = rng.standard_normal((n, self.d))
+        else:
+            soboleng = qmc.Sobol(self.d, scramble=True, seed=seed)
+            X = ndtri(soboleng.random(n) * (1 - MACHINE_EPSILON) + .5 * MACHINE_EPSILON)
+        Z_nf, log_det = self.forward_and_logdet(X)
+        if not return_weights:
+            return Z_nf
+        log_q = -0.5 * jnp.sum(X**2, axis=-1) - 0.5 * self.d * jnp.log(2 * jnp.pi)
+        proposal_log_densities = log_q - log_det
+        target_log_densities = self.target.log_prob(Z_nf)
+        weights = np.exp(target_log_densities - proposal_log_densities)
+        return Z_nf, weights
+
+def train(nf, rng, rqmc=True, max_iter=100, lr=None, nsample=2**10, fix_samples=True):
+    d = nf.target.d
+    params = nf.pack_params()
+    optimizer = optax.lbfgs(learning_rate=lr)
+    opt_state = optimizer.init(params)
+
+    @jax.jit
+    def step(params, opt_state, x):
+        loss, grad = jax.value_and_grad(nf.reverse_kld)(params, x)
+        updates, opt_state = optimizer.update(grad, opt_state, params, value=loss, grad=grad, value_fn=lambda params: nf.reverse_kld(params, x))
+        params = optax.apply_updates(params, updates)
+        return params, opt_state, loss
+
+    if rqmc:
+        soboleng = qmc.Sobol(d, scramble=True, seed=rng)
+        def sample(n):
+            return ndtri(soboleng.random(n) * (1 - MACHINE_EPSILON) + .5 * MACHINE_EPSILON)
+    else:
+        sample = lambda n: rng.standard_normal((n, d))
+    if fix_samples:
+        x = sample(nsample)
+    losses = []
+    pbar = trange(max_iter)
+    for t in pbar:
+        if not fix_samples:
+            x = sample(nsample)
+        params, opt_state, loss = step(params, opt_state, x)
+        losses.append(loss.item())
+        pbar.set_description(f"Loss: {loss.item()}")
+    return params, losses
+
