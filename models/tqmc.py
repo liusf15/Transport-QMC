4bd73f4868a8e96f0470d2b03a955c94cb222dc0
liusf15 <sliu@flatironinstitute.org>
2024-09-29 09:36:04 -0400

    inverse

--- models/tqmc.py
+++ models/tqmc.py
@@ -7,6 +7,7 @@ from jax.nn import sigmoid
 from jax.scipy.stats.beta import pdf as beta_pdf
 from jax.nn import softmax, sigmoid
 import jax
+from scipy.optimize import root_scalar
 
 from typing import NamedTuple
 
@@ -19,6 +20,27 @@ def mixture_beta_cdf(x, shapes, weights):
 
 def mixture_beta_log_pdf(x, shapes, weights):
     return jnp.log(jnp.dot(beta_pdf(x, shapes[:,0], shapes[:, 1]), softmax(weights)))
+
+@jax.jit
+def mixture_beta_inverse_cdf(q, shapes, weights):
+    ws = softmax(weights)
+    def h(x):
+        return jnp.dot(betainc(shapes[:,0], shapes[:, 1], x), ws)
+    def h_prime(x):
+        return jnp.dot(beta_pdf(x, shapes[:,0], shapes[:, 1]), ws)
+    tol = jnp.finfo(jnp.float32).eps
+    def cond_fn(state):
+        x, itercount = state
+        return jnp.logical_and(jnp.abs(h(x) - q) > tol, itercount < 20)
+    def step_fn(state):
+        x, itercount = state
+        return x - (h(x) - q) / h_prime(x), itercount + 1
+    x = q
+    x, itercount = jax.lax.while_loop(cond_fn, step_fn, (x, 0))
+    return x
+
+# q = mixture_beta_cdf(0.7, shapes=np.array([[1, 1], [2, 1]]), weights=np.array([0., 0.]))
+# mixture_beta_inverse_cdf(q, shapes=np.array([[1, 1], [2, 1]]), weights=np.array([0., 0.]))
 
 class TransportQMC:
     def __init__(self, d, target, base_transform='logit', nonlinearity='logit', num_composition=1, max_deg=3):
@@ -93,6 +115,40 @@ class TransportQMC:
 
         for p in params:
             x, log_det_ = self.forward_one_layer(p, x)
+            log_det += log_det_
+        return x, log_det
+    
+    def elementwise_inverse(self, weights, x):
+        log_det = jnp.sum(jnp.log(self.F_grad(x)))
+        x = self.F(x)
+        
+        x = jax.vmap(mixture_beta_inverse_cdf, in_axes=[0, None, 0])(x, self.shapes, weights)
+        log_det += -jnp.sum(jax.vmap(mixture_beta_log_pdf, in_axes=[0, None, 0])(x, self.shapes, weights))
+
+        eps = jnp.finfo(jnp.float32).eps
+        x = jnp.clip(x, eps * .5, 1 - eps * .5)
+        log_det += jnp.sum(jnp.log(self.F_inv_grad(x)))
+        x = self.F_inv(x)
+        return x, log_det
+
+    def inverse_one_layer(self, params, x):
+        weights = params['weights']
+        L = jnp.diag(jnp.exp(params['L'][:self.d]))
+        mask = np.tri(self.d, k=-1, dtype=bool)
+        L = L.at[mask].set(params['L'][self.d:])
+        b = params['b']        
+
+        x, log_det_ = self.elementwise_inverse(weights, x)
+        log_det = log_det_
+
+        x = jax.scipy.linalg.solve_triangular(L, x - b, lower=True)
+        log_det += -jnp.sum(params['L'][:self.d])
+        return x, log_det
+
+    def inverse(self, params, x):
+        log_det = 0.
+        for p in reversed(params):
+            x, log_det_ = self.inverse_one_layer(p, x)
             log_det += log_det_
         return x, log_det
 
